c877e6c49b73e451b30601739ea7c7c1346965c5
dhairya0512 <75428863+dhairya0512@users.noreply.github.com>
2022-06-03 14:29:18 +0530

    init commit

---
+++ app/src/main/java/dhairyapandya/com/authapp/MainActivity.java
@@ -0,0 +1,30 @@
+package dhairyapandya.com.authapp;
+
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+EditText email,password;
+Button login,register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email = findViewById(R.id.Emailbtn);
+        login = findViewById(R.id.Login);
+        register = findViewById(R.id.Register);
+        password = findViewById(R.id.Password);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+    }
+}
